35220ea3cf0ba9c5a456f66c4d0a2cd21b23aac4
Jonathan Qian <118020116+Jonathan-Qian@users.noreply.github.com>
2024-11-14 22:30:09 -0500

    Added dynamic scale which tries to predict the number of calculation digits that are correct

--- Main.java
+++ Main.java
@@ -6,21 +6,41 @@ import java.math.RoundingMode;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("start");
+		System.out.println("Start");
+		int iterations = 100000;
 		long end, start = System.currentTimeMillis();
-		BigDecimal calculation = calculate(100000);
+		BigDecimal calculation = calculate(iterations);
 		end = System.currentTimeMillis();
 		System.out.println();
 		System.out.println("Execution time: " + ((end - start) / 1000.0) + " seconds");
 		check(calculation);
+		System.out.println(iterations * (-0.417 + 0.433 * Math.log(iterations * 1.0)));
 		System.out.println();
+
+		//int correctDigits;
+
+		/*for (int i = 0; i <= 200000; i += 100) {
+			System.out.println(i);
+			calculation = calculate(i);
+			correctDigits = check(calculation);
+			System.out.println();
+
+			try {
+				PrintWriter writer = new PrintWriter(new FileWriter("correctDigits.csv", true));
+
+				writer.println(i + "," + correctDigits);
+
+				writer.close();
+			}
+			catch (Exception e) {
+				System.err.println(e);
+			}
+		}*/
 	}
 	
 	public static BigDecimal calculate(int iterations) {
 		final BigDecimal ONE = new BigDecimal(1);
-		BigDecimal numerator = new BigDecimal(2),
-				denominator = ONE,
-				bdi;
+		BigDecimal numerator = new BigDecimal(2), denominator = ONE, bdi;
 		
 		
 		for (int i = 2; i <= iterations; i++) {
@@ -29,21 +49,15 @@ public class Main {
 			denominator = denominator.multiply(bdi);
 		}
 		
-		/*int scale;
+		double scale = iterations * (-0.417 + 0.433 * Math.log(iterations * 1.0));
+		scale -= 0.005 * (iterations - 500); // comment this line out if you don't want to have more digits than are correct instead of less digits than are correct
 		
-		if (iterations < 15) {
-			scale = iterations - 3;
-		}
-		else {
-			scale = (int) (-0.00000452 * Math.pow(iterations, 3.0) + 0.00376 * Math.pow(iterations, 2.0) + 1.36 * iterations - 8.5) - 1;
-		}*/
-		
-		return numerator.divide(denominator, 2000000, RoundingMode.FLOOR);
+		return numerator.divide(denominator, (int) (scale), RoundingMode.FLOOR);
 	}
 	
-	public static void check(BigDecimal calculation) {
+	public static int check(BigDecimal calculation) {
 		try {
-			System.out.println(calculation);
+			//System.out.println(calculation);
 			
 			BufferedReader reader = new BufferedReader(new FileReader("digits.txt"));
 			
@@ -76,11 +90,20 @@ public class Main {
 			}
 			
 			System.out.println("Consecutive correct digits: " + correctDigits);
-			calculation = calculation.setScale(correctDigits - 1, RoundingMode.FLOOR);
-			System.out.println(calculation);
+
+			if (calculationDigits == correctDigits) {
+				System.out.println("Scale is probably too small.");
+			}
+
+			//calculation = calculation.setScale(correctDigits - 1, RoundingMode.FLOOR);
+			//System.out.println(calculation);
+
+			return correctDigits;
 		}
 		catch (IOException e) {
 			System.err.println(e);
 		}
+
+		return 0;
 	}
 }
